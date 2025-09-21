de35115e7fa7a55a7d0d51fc7236651a3e4761a8
rlucatoor <rlucato95@gmail.com>
2025-09-21 14:57:05 +0200

    remove unused layouts

--- src/layouts/BlogPageLayout/index.jsx
+++ src/layouts/BlogPageLayout/index.jsx
@@ -1,14 +1,9 @@
-import React from 'react';
-
 import './index.scss';
-import { HeroSection } from './HeroSection';
-import { ArticlesSection } from './ArticlesSection';
 
 
 export const BlogPageLayout = ({ thumbnails }) => {
     return <div>
-        <HeroSection />
-        <ArticlesSection />
+        test
     </div>
 };
 
