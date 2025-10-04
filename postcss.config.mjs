f2e2c9efa02a2882e70356f8d861a3b38e4eca49
rlucatoor <rlucato95@gmail.com>
2025-10-04 11:45:03 +0200

    add postcss.config.mjs

---
+++ postcss.config.mjs
@@ -0,0 +1,28 @@
+export default {
+  plugins:
+    [
+        'postcss-flexbugs-fixes',
+        [
+            'postcss-preset-env',
+            {
+                autoprefixer: {
+                    flexbox: 'no-2009',
+                },
+                stage: 3,
+                features: {
+                    'custom-properties': false,
+                },
+            },
+        ],
+        [
+            '@fullhuman/postcss-purgecss',
+            {
+                content: [
+                    './app/**/*.{js,jsx,ts,tsx}',
+                    './components/**/*.{js,jsx,ts,tsx}',
+                ],
+                defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+            }
+        ],
+    ]
+}
