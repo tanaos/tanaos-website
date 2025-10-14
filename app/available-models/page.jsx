94c5e161bf6ffa54fb9333a90de7e6929664fffb
rlucatoor <rlucato95@gmail.com>
2025-10-14 18:14:39 +0200

    add service to fetch all available modles

--- app/available-models/page.jsx
+++ app/available-models/page.jsx
@@ -1,19 +1,9 @@
 import styles from './AvailableModels.module.scss';
 import { CodeSnippet } from '../../components/CodeSnippet';
+import { getAvailableModels } from '../../lib/availableModels';
 
 
-const sampleCode = `from artifex import Artifex
-
-guardrail = Artifex().guardrail
-
-guardrail.train(
-    instructions=[
-        "Soft medical advice is allowed, but it should be general and not specific to any individual.",
-        "Anything that is about cosmetic products, including available products or their usage, is allowed.",
-        "Anything else, including hard medical advice, is not allowed under any circumstances.",
-    ]
-)
-`;
+const availableModels = await getAvailableModels();
 
 
 export const AvailableModels = () => {
@@ -24,18 +14,19 @@ export const AvailableModels = () => {
                 Explore our collection of task-specific models designed to enhance your AI applications. 
             </p>
         </div>
-        <div className='mt-5'>
-            <div className={styles['model-box']}>
-                <h2>🛡️ Guardrail Model</h2>
-                <p className='mt-2'>
-                    Guardrail models are used to flag unsafe, harmful or off-topic messages in chatbots 
-                    and virtual assistants.
-                </p>
-                <CodeSnippet
-                    code={sampleCode}
-                />
+        {availableModels.map(({ name, description, emoji, sampleCode }) => (
+            <div key={name} className='mt-5'>
+                <div className={styles['model-box']}>
+                    <h2>{emoji} {name}</h2>
+                    <p className='mt-2'>
+                        {description}
+                    </p>
+                    <CodeSnippet
+                        code={sampleCode}
+                    />
+                </div>
             </div>
-        </div>
+        ))}
     </div>
 }
 
