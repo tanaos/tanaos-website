3f91f8b7379aacd7b063c68b6fec27459d870f26
rlucatoor <rlucato95@gmail.com>
2025-10-14 18:19:00 +0200

    replace hardcoded available model information with dynamic one

--- app/sections/AvailableModels.jsx
+++ app/sections/AvailableModels.jsx
@@ -3,6 +3,10 @@ import Image from 'next/image';
 
 import { Config } from '../../config';
 import styles from './AvailableModels.module.scss';
+import { getAvailableModels } from '../../lib/availableModels';
+
+
+const availableModels = await getAvailableModels();
 
 
 export const AvailableModels = () => {
@@ -10,88 +14,37 @@ export const AvailableModels = () => {
     return <div id='available-models' className='global-padding'>
         <div className='row'>
             <h2 className='mb-5'>Available Task-Specific Models</h2>
-            <div className='col-12 col-md-4 margin-end'>
-                <div className={`${styles['model-card']}`}>
-                    <Image 
-                        src='/images/available-models/guardrail.png'
-                        alt='Easily create Guardrail Models without training data or GPUs | Tanaos'
-                        width={547} height={132}
-                        className={styles['model-image']}
-                        unoptimized
-                    />
-                    <h3 className='mb-3'>Guardrail Model</h3>
-                    <p>
-                        Guardrail models are used to flag unsafe, harmful or off-topic messages 
-                        in chatbots and virtual assistants.
-                    </p>
-                    <div className='mt-4'>
-                        <a 
-                            href={Config.DOCS_GUARDRAIL_INTRO_URL}
-                            target='_blank' rel='noreferrer'
-                            className='btn btn-white me-4'
-                        >
-                            Read the docs <BoxArrowUpRight className='ms-1' />
-                        </a>
-                        <a 
-                            href={Config.ARTIFEX_GUARDRAIL_DEMO_URL}
-                            target='_blank' rel='noreferrer'
-                            className='btn btn-white'
-                        >
-                            Try it out <BoxArrowUpRight className='ms-1' />
-                        </a>
+            {availableModels.map(({ name, description, imageUrl }) => (
+                <div className='col-12 col-md-4 margin-end' key={name}>
+                    <div className={`${styles['model-card']}`}>
+                        <Image 
+                            src={imageUrl}
+                            alt={`Easily create ${name}s without training data or GPUs | Tanaos`}
+                            width={547} height={132}
+                            className={styles['model-image']}
+                            unoptimized
+                        />
+                        <h3 className='mb-3'>{name}</h3>
+                        <p>{description}</p>
+                        <div className='mt-4'>
+                            <a 
+                                href={Config.DOCS_GUARDRAIL_INTRO_URL}
+                                target='_blank' rel='noreferrer'
+                                className='btn btn-white me-4'
+                            >
+                                Read the docs <BoxArrowUpRight className='ms-1' />
+                            </a>
+                            <a 
+                                href={Config.ARTIFEX_GUARDRAIL_DEMO_URL}
+                                target='_blank' rel='noreferrer'
+                                className='btn btn-white'
+                            >
+                                Try it out <BoxArrowUpRight className='ms-1' />
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className='col-12 col-md-4 margin-end mt-5 mt-md-0'>
-                <div className={`${styles['model-card']}`}>
-                    <Image 
-                        src='/images/available-models/intent-classification.png'
-                        alt='Easily create Intent Classification Models without training data or GPUs | Tanaos'
-                        width={547} height={132}
-                        className={styles['model-image']}
-                        unoptimized
-                    />
-                    <h3 className='mb-3'>Intent Classification</h3>
-                    <p>
-                        Intent Classification models are used in chatbots and virtual assistants 
-                        to classify user intents.
-                    </p>
-                    <div className='mt-4'>
-                        <a 
-                            href={Config.DOCS_INTENT_CLASSIFICATION_INTRO_URL}
-                            className='btn btn-white me-4'
-                            target='_blank' rel='noreferrer'
-                        >
-                            Read the docs <BoxArrowUpRight className='ms-1' />
-                        </a>
-                        <a
-                            href={Config.ARTIFEX_INTENT_CLASSIFIER_DEMO_URL}
-                            target='_blank' rel='noreferrer'
-                            className='btn btn-white'
-                        >
-                            Try it out <BoxArrowUpRight className='ms-1' />
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div className='col-12 col-md-4 margin-end mt-5 mt-md-0'>
-                <div className={`${styles['model-card']}`}>
-                    <Image 
-                        src='/images/available-models/reranker.png'
-                        alt='Easily create Intent Classification Models without training data or GPUs | Tanaos'
-                        width={547} height={132}
-                        className={styles['model-image']}
-                        unoptimized
-                    />
-                    <h3 className='mb-3'>Reranker</h3>
-                    <p>
-                        Reranker models improve the relevance of search results by reordering them 
-                        based on their relevance to a given query.
-                    </p>
-                    <div className='mt-4'>
-                    </div>
-                </div>
-            </div>
+            ))}
         </div>
     </div>
 };
