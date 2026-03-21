92a2a944cac2de2528c16368ed159fad6c9ccd8b
rlucatoor <rlucato95@gmail.com>
2026-03-21 11:01:10 +0100

    remove unused footer section

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -16,16 +16,6 @@ export const Footer = () => {
                         <a href={Config.PLATFORM_BASE_URL} rel='noreferrer' target='_blank'>Platform</a>
                     </div>
                     <div className={styles['footer-nav-column']}>
-                        <div className={styles['footer-nav-title']}>Models</div>
-                        <a href={Config.MODELS_TICKET_CLASSIFICATION_ROUTE}>Ticket Classification</a>
-                        <a href={Config.MODELS_CONTACT_FORM_SPAM_FILTER_ROUTE}>Contact Form Spam Filter</a>
-                        <a href={Config.MODELS_EMAIL_INTENT_DETECTION_ROUTE}>Email Intent Detection</a>
-                        <a href={Config.MODELS_CHATBOT_SAFETY_MODERATION_ROUTE}>Chatbot Safety & Moderation</a>
-                        <a href={Config.MODELS_BLOG_POSTS_MODERATION_ROUTE}>Blog Posts Moderation</a>
-                        <a href={Config.MODELS_ANOMALY_FRAUD_DETECTION_ROUTE}>Anomaly & Fraud Detection</a>
-                        <a href={Config.MODELS_PREDICTIVE_MAINTENANCE_ROUTE}>Predictive Maintenance</a>
-                    </div>
-                    <div className={styles['footer-nav-column']}>
                         <div className={styles['footer-nav-title']}>Socials</div>
                         <a href={Config.GITHUB_URL} rel='noreferrer' target='_blank' aria-label='GitHub'>
                             <BsGithub style={{ verticalAlign: 'middle', marginRight: 8 }} /> GitHub
